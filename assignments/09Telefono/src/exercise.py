7012c60ad7b05b16645b17bf0f974c4e5957f8da
ImanolCS <89592199+ImanolCS@users.noreply.github.com>
2021-08-26 20:59:43 -0500

    Subido
    Lo subi desde explorador de archivos aun no se como subirlo ajsjs

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,7 +1,13 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    mens= int(input("Dame el numero de mensajes: "))
+    megas= float(input("Dame el numero de megas: "))
+    min= int(input("Dame el numero de minutos: "))
 
+    
+    costo =  (mens + megas + min)*0.8
+    round(costo, 2)
+    print("El costo mensual es:", costo)
 
 if __name__ == '__main__':
     main()
