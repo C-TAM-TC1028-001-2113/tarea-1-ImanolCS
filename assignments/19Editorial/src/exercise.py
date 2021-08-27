c455c744136084c2c897e94f78feb9a2b751df15
ImanolCS <89592199+ImanolCS@users.noreply.github.com>
2021-08-26 21:25:41 -0500

    Subido 4
    Lo subi desde explorador de archivos

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,6 +1,13 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    from math import ceil 
+    pal=int(input("Dame el número de palabras: "))
+
+    pal = ceil(pal/475) 
+    precio = pal*60*(0.9) 
+
+    print("El costo de la publicación es:", precio)
+
 
 
 if __name__ == '__main__':
