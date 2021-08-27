bb5589aac15a5149b986027767871c54589981a7
ImanolCS <89592199+ImanolCS@users.noreply.github.com>
2021-08-26 23:21:42 -0500

    Modificacion
    ajaja subí antes de guardarlo que menso

--- assignments/21LevaduraParaPizza/src/exercise.py
+++ assignments/21LevaduraParaPizza/src/exercise.py
@@ -1,6 +1,13 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    
+     #Se requieren 50 gramos de levadura para 1 kg. de harina si se utiliza para la base de una pizza.
+     #Realiza
+     #el cálculo de la levadura necesitada a partir de los gramos de harina que indique el usuario.
+     h= float(input("dame harina en gramos: "))
+
+     levadura = h*(50/1000)
+     print("Necesitas estos gramos de levadura:", levadura )
 
 
 if __name__ == '__main__':
