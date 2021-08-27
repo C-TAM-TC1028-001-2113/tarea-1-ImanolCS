4ed72c0921edb945e9259989416d9f4376342ffc
ImanolCS <89592199+ImanolCS@users.noreply.github.com>
2021-08-26 21:00:16 -0500

    Subido 2
    Lo subi desde explorador de archivos

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    JN = int(input("Dame la cantidad de juegos nuevos: "))
+    JU = int(input("Dame la cantidad de juegos usados: "))
+
+    total = (JN*1000) + (JU*350) 
+    print("El total de la compra es:", total)
 
 
 if __name__ == '__main__':
