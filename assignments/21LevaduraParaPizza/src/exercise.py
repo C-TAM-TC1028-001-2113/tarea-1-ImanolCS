7b003b4b5853f314ad8806edfaafac950e69332e
ImanolCS <89592199+ImanolCS@users.noreply.github.com>
2021-08-29 22:16:52 -0500

    Otro cambio
    Solo el texto, lo probé con otros valores y ya debería de dar lo correcto, espero

--- assignments/21LevaduraParaPizza/src/exercise.py
+++ assignments/21LevaduraParaPizza/src/exercise.py
@@ -4,7 +4,7 @@ def main():
      #Se requieren 50 gramos de levadura para 1 kg. de harina si se utiliza para la base de una pizza.
      #Realiza
      #el cálculo de la levadura necesitada a partir de los gramos de harina que indique el usuario.
-     h= float(input("dame harina en gramos: "))
+     h= float(input("Dame la harina en gramos: "))
 
      levadura = h*(50/1000)
      print("Necesitas estos gramos de levadura:", levadura )
